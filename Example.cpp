2283c8d6c7467af929276dfbf46cfe273a422ea4
CodeMouse179 <codemouse179@gmail.com>
2023-03-12 14:48:51 +0300

    change c++ minimum required version and update example

--- Example.cpp
+++ Example.cpp
@@ -1,6 +1,22 @@
 #include "System.hpp"
+#include <iostream>
 
 int main()
 {
+#ifdef SYSTEM_HPP
+    std::cout << "Example started.\n";
+    std::cout << "System version : " << SYSTEM_VERSION_STRING << "\n";
+    std::cout << "Current platform : ";
+#ifdef SYSTEM_WINDOWS
+    std::cout << "Windows";
+#endif
+#ifdef SYSTEM_LINUX
+    std::cout << "Linux";
+#endif
+#ifdef SYSTEM_MACOS
+    std::cout << "macOS";
+#endif
+    std::cout << "\nExample finished.\n";
+#endif
     return 0;
 }
