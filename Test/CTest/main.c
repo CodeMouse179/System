6bfde7badbc766a899eb80d24f02973c3cfb24db
CodeMouse179 <codemouse179@gmail.com>
2023-04-26 18:02:05 +0300

    update Example in c and c++

--- Test/CTest/main.c
+++ Test/CTest/main.c
@@ -3,43 +3,31 @@
 // Platform : Windows, Linux, macOS
 
 #include "System.hpp"
-
-#ifdef SYSTEM_CXX
-#include <iostream>
-void write(const char* str)
-{
-    std::cout << str;
-}
-void write_line(const char* str)
-{
-    std::cout << str << "\n";
-}
-#else
 #include <stdio.h>
+
 void write(const char* str)
 {
-    printf(str);
+    printf("%s", str);
 }
+
 void write_line(const char* str)
 {
-    printf(str);
-    printf("\n");
-}
-#endif
-
-void system_start()
-{
-    write_line("----- System Example Started -----");
+    printf("%s\n", str);
 }
 
-void system_end()
+void example_start()
 {
-    write_line("----- System Example Finished ----");
+    write_line("----- Example Started -----");
 }
 
-void print_system_hpp_version()
+void example_end()
 {
-    write("1.System Version   : ");
+    write_line("----- Example Finished ----");
+}
+
+void print_version()
+{
+    write("1.System Version    : ");
 #ifdef SYSTEM_HPP
     write(SYSTEM_VERSION_STRING);
 #endif
@@ -48,7 +36,7 @@ void print_system_hpp_version()
 
 void print_current_platform()
 {
-    write("2.Current Platform : ");
+    write("2.Current Platform  : ");
 #ifdef SYSTEM_WINDOWS
     write("Windows");
 #endif
@@ -74,7 +62,7 @@ int c_or_cpp()
 
 void print_current_standard()
 {
-    write("3.Current Standard : ");
+    write("3.Current Standard  : ");
 #ifdef SYSTEM_CXX_20
     write_line("C++ 20");
     return;
@@ -100,7 +88,7 @@ void print_current_standard()
 
 void print_current_c_standard()
 {
-    write("3.Current CStandard: ");
+    write("3.Current CStandard : ");
 #ifdef SYSTEM_C_17
     write_line("C 17");
     return;
@@ -122,7 +110,7 @@ void print_current_c_standard()
 
 void print_current_compiler()
 {
-    write("4.Current Compiler : ");
+    write("4.Current Compiler  : ");
 #ifdef SYSTEM_MSC
     write("Microsoft C/C++ Compiler");
 #endif
@@ -138,14 +126,14 @@ void print_current_compiler()
 int main()
 {
 #ifdef SYSTEM_HPP
-    system_start();
-    print_system_hpp_version();
+    example_start();
+    print_version();
     print_current_platform();
     int r = c_or_cpp();
     if (r == 1) print_current_c_standard();
     if (r == 2) print_current_standard();
     print_current_compiler();
-    system_end();
+    example_end();
 #endif
     return 0;
 }
